d4af7f81ad1555357e29486acc43fae2c24b2d45
Nicolas Agustin Torres <nicolastrres@gmail.com>
2016-03-01 21:55:30 -0300

    Adding logic to get scenarios

--- application/feature_file_parser/parse_feature_files.py
+++ application/feature_file_parser/parse_feature_files.py
@@ -1,7 +1,43 @@
 def get_feature(feature_file_path):
     feature_label = 'Feature:'
     with open(feature_file_path) as file:
-
         for line in file:
             if feature_label in line:
                 return line[:-1]
+
+
+def get_scenarios(file_lines):
+    scenario_label = 'Scenario:'
+    file_lines = _remove_spaces_in_lines(file_lines)
+    scenarios_start_line_number = _get_first_line_number_of_each_scenario(file_lines, scenario_label)
+
+    scenarios = []
+    for index, start_line_number in enumerate(scenarios_start_line_number):
+        if is_not_last_scenario(index, scenarios_start_line_number):
+            scenarios.append(_get_string_scenario(file_lines, start_line_number, scenarios_start_line_number[index+1]))
+        else:
+            scenarios.append(_get_string_scenario(file_lines, start_line_number))
+    return scenarios
+
+
+def is_not_last_scenario(index, scenarios):
+    return index + 1 < len(scenarios)
+
+
+def _get_first_line_number_of_each_scenario(file_lines, scenario_label):
+    scenarios_start_line_number = []
+    for line in file_lines:
+        if scenario_label in line:
+            scenarios_start_line_number.append(file_lines.index(line))
+
+    return scenarios_start_line_number
+
+
+def _get_string_scenario(file_lines, start_line, last_line=None):
+    scenario_lines = file_lines[start_line: last_line]
+    string_scenario = ''.join(scenario_lines)
+    return string_scenario
+
+
+def _remove_spaces_in_lines(file_lines):
+    return [line.lstrip(' ') for line in file_lines]
--- application/tests/test_parse_feature_files.py
+++ application/tests/test_parse_feature_files.py
@@ -1,7 +1,23 @@
-from hamcrest import assert_that, equal_to
-from application.feature_file_parser.parse_feature_files import get_feature
+from hamcrest import assert_that, equal_to, has_length
+
+from application.feature_file_parser.parse_feature_files import get_feature, get_scenarios
 
 
 def test_get_feature_from_file():
     feature = get_feature('test.feature')
     assert_that(feature, equal_to('Feature: Login into social network'))
+
+
+def test_get_scenarios():
+    expected_scenario = 'Scenario: Login with valid credentials\n' \
+                        'Given I access to the social network home page\n' \
+                        'When I enter my my email and password and login\n' \
+                        'Then I can see my beatiful picture\n' \
+                        'And I can see a welcome message\n'
+
+    with open('test.feature') as file:
+        file_lines = file.readlines()
+        scenarios = get_scenarios(file_lines)
+
+        assert_that(scenarios, has_length(1))
+        assert_that(scenarios[0], equal_to(expected_scenario))
