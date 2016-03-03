0570041d2051c546446e4faa33e660c15288e6e3
Nicolas Agustin Torres <nicolastrres@gmail.com>
2016-03-02 23:37:44 -0300

    Adding basic template and sending the feature title

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 
-from flask import Flask
+from flask import Flask, render_template
+from feature_file_parser import parse_feature_files
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Features Report'
+    test_file = 'tests/test.feature'
+    with open(test_file) as file:
+        lines = file.readlines()
+        feature = parse_feature_files.get_feature(lines)
+    return render_template('index.html', feature=feature)
 
 
 if __name__ == '__main__':
