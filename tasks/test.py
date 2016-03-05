ce047bf52e67e198675d784a53499e0d7d548cda
Nicolas Agustin Torres <nicolastrres@gmail.com>
2016-03-05 19:38:11 -0300

    Adding paver and creating paver task to run tests and flake8

---
+++ tasks/test.py
@@ -0,0 +1,17 @@
+from paver.easy import task, sh, call_task
+
+
+@task
+def test_and_code_style():
+    call_task('flake8')
+    call_task('unit_test')
+
+
+@task
+def unit_test():
+    sh('nosetests')
+
+
+@task
+def flake8():
+    sh('flake8')
