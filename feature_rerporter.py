c8a4d5427b15f13c43d3bac6bd756d4033963ebb
Nicolas Agustin Torres <nicolastrres@gmail.com>
2016-03-13 22:34:14 -0300

    Removing logic to save the file, and reading the file directly

--- feature_rerporter.py
+++ feature_rerporter.py
@@ -1,7 +1,4 @@
-import os
-
 from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
 
 from feature_file_parser import parse_feature_files
 
@@ -22,14 +19,10 @@ def index():
 def upload():
     if request.method == 'POST' and 'feature_file' in request.files:
         file = request.files['feature_file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-            with open(file_path) as file:
-                lines = file.readlines()
-                feature = parse_feature_files.get_feature(lines)
-                scenarios = parse_feature_files.get_scenarios(lines)
+        if allowed_file(file.filename):
+            lines = [line.decode('utf-8') for line in file.readlines()]
+            feature = parse_feature_files.get_feature(lines)
+            scenarios = parse_feature_files.get_scenarios(lines)
             return render_template('index.html', feature=feature, scenarios=scenarios)
 
 
@@ -38,10 +31,8 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-UPLOAD_FOLDER = '/tmp'
 ALLOWED_EXTENSIONS = {'feature', 'txt'}
 
 
 if __name__ == '__main__':
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.run(debug=True)
