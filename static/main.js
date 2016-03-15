b8bcc83720d62b84eb33f6d949ddbfa6a3bc926e
Nicolas Agustin Torres <nicolastrres@gmail.com>
2016-03-14 21:09:29 -0300

    Adding logic to set features_nav height equal of scenarios height

--- static/main.js
+++ static/main.js
@@ -1,5 +1,11 @@
 window.onload = function () {
     document.getElementById("file").onchange = function () {
         document.getElementById("upload_form").submit();
+    };
+
+    var scenarios = document.getElementById("scenarios");
+    if(scenarios.offsetHeight > document.documentElement.offsetHeight){
+        document.getElementById("features_nav").style.height = scenarios.offsetHeight+'px';
     }
 };
+
