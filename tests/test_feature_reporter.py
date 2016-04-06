327622e16b86771efc00513fcb9886bb3eeb2451
Nicolas Agustin Torres <nicolastrres@gmail.com>
2016-04-05 23:11:10 -0300

    Removing unnecesary lines

--- tests/test_feature_reporter.py
+++ tests/test_feature_reporter.py
@@ -35,15 +35,7 @@ class FeatureReporterTestCase(unittest.TestCase):
         assert_that('Feature: Login into social network' in str(rv.data))
 
     def test_upload_file_with_invalid_extension(self):
-        lines = b'Feature: Login into social network \
-                As an User\
-                I want to be able to login\
-                So I can share my personal information with my friends (FBI, NSA, and US government\
-                Scenario: Login with valid credentials\
-                Given I access to the social network home page\
-                When I enter my email and password and login\
-                Then I can see my beatiful picture\
-                And I can see a welcome message'
+        lines = b'crazy line'
         file = (io.BytesIO(lines), 'my_feature_test.txt')
 
         rv = self.app.post('/upload', data=dict(feature_file=file), follow_redirects=True)
